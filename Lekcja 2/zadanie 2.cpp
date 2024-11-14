466e2ed74364fee4acb79aeb03c8320eb5f59117
Matthew-exe <mborowski679@technikumkreatywne.pl>
2024-11-14 09:51:08 +0100

    zadanie 2 zakończone

--- Lekcja 2/zadanie 2.cpp
+++ Lekcja 2/zadanie 2.cpp
@@ -3,9 +3,14 @@ using namespace std;
 int main () {
 	int waga;
 	int wzrost;
+	int BMI;
 	cout << "podaj wage (w kg):";
 	cin >> waga;
 	cout << "podaj wzrost (w metrach):";
 	cin >> wzrost;
-	cout << "W.I.P";
+	BMI = waga / (wzrost * wzrost);
+	if(BMI < 18.5) cout << "masz niedowage";
+	else if((BMI > 18.5) && (BMI <= 24.9)) cout << "w normie";
+	else if((BMI > 25) && (BMI < 29.9)) cout << "masz nadwage";
+	else if(BMI > 30) cout << "otylosc";	
 }
