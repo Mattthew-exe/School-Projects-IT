34ad70249b391a2835e4ece8f2e63f2c58f03145
Matthew-exe <mborowski679@technikumkreatywne.pl>
2025-01-23 10:10:58 +0100

    Added the 4th assignment for the 6th lesson

--- Lekcja 6/Zadanie 3.cpp
+++ Lekcja 6/Zadanie 3.cpp
@@ -10,9 +10,10 @@ int main(){
 	// chce zrobić aby skrypt sprawdzał czy liczba jest parzysta i aby potem te liczbę DODAŁ do sumy
 	cout << "liczby parzyste: ";
 	for(int i = 0; i < 10; i++){
-		if(tablica[i] % 2 = 0);
-		cout << tablica[i];
-		suma += tablica[i];
+		if(tablica[i] % 2 == 0){
+		cout << tablica[i] << endl;
+		suma = suma += tablica[i];
+	}
 	}
 	cout << "Suma liczb parzystych: " << suma << endl;
 	
---
+++ Lekcja 6/Zadanie 4.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int macierz[3][3];
+	int srednia = 0;
+	cout << "Podaj 9 liczb dla macierzy 3x3: " << endl;
+	for(int i = 0; i < 3; i++){
+		for(int j = 0; j < 3; j++){
+			cout << "Podaj liczbe wiersza " << i + 1 << " Kolumny " << j + 1 << ": ";
+			cin >> macierz[i][j];
+		}
+	}
+	for(int i = 0; i < 3; i++){
+		for(int j = 0; j < 3; j++){
+		 srednia = srednia + macierz[i][j];
+		}
+		cout << endl;
+	}
+	srednia = srednia / 9;
+	cout << "srednia liczb jest ruwna: " << srednia << endl;
+}
