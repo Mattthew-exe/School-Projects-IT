414a98105c30e6b6625dca40a13037f20c74e7fc
Matthew-exe <mborowski679@technikumkreatywne.pl>
2025-04-01 11:59:46 +0200

    added the 3rd assignment, i'm confusion

---
+++ Lekcja 8 - funkcje/Zadanie 3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+		string GetTextFromUser(){
+			string text;
+			string reversed;
+			getline(cin, text);
+			return text;
+		}
+			string ReverseString(char text[]){
+					string reversed;
+					strlen(text);
+				}
+int main(){
+	string text;
+	cout << "podaj ciag znakow: ";
+	text = GetTextFromUser();
+}
