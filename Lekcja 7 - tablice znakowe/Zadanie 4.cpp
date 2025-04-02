ede1c105bbc3afeb9edbf099bc487c162c1164a5
Matthew-exe <mborowski679@technikumkreatywne.pl>
2025-04-02 14:07:43 +0200

    tried finishing the 4th assignment in the 7th lesson

---
+++ Lekcja 7 - tablice znakowe/Zadanie 4.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstring>
+#include <string>
+using namespace std;
+int main(){
+	 string text;
+	 cout << "Podaj ciag znakow: ";
+	 getline(cin, text);
+	 for(int i; ; i++){
+	 	
+	}
+}
